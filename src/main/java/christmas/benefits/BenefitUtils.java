187b6bade261a9ca250ebf79068a968ee09c9747
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 00:49:14 +0900

    feat(BenefitUtils) : 총주문 금액이 10,000원 이상인 고객이니 확인하는 기능 구현

--- src/main/java/christmas/benefits/BenefitUtils.java
+++ src/main/java/christmas/benefits/BenefitUtils.java
@@ -1,4 +1,10 @@
 package christmas.benefits;
 
 public class BenefitUtils {
+
+    private final int EVENT_TARGET_CONDITION = 10_000;
+
+    public boolean isEventTarget(int totalOrder){
+        return (totalOrder >= EVENT_TARGET_CONDITION);
+    }
 }
--- src/main/java/christmas/benefits/MerchandiseBenefit.java
+++ src/main/java/christmas/benefits/MerchandiseBenefit.java
@@ -5,9 +5,6 @@ public class MerchandiseBenefit {
     private final int CHAMPAGNE_DESERVED = 120_000;
 
     public boolean champagneDeserved(int totalOrder){
-        if(totalOrder > CHAMPAGNE_DESERVED){
-            return true;
-        }
-        return false;
+        return totalOrder > CHAMPAGNE_DESERVED;
     }
 }
