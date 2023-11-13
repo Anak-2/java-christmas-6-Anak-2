d681ff57b4a5f94d1eff9cce324819bfe94aa951
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 00:45:13 +0900

    feat(MerchandiseBenefit) : 할인 전 주문 금액이 12만원 이상일 때 샴페인 증정하는 기능 구현

--- src/main/java/christmas/benefits/MerchandiseBenefit.java
+++ src/main/java/christmas/benefits/MerchandiseBenefit.java
@@ -2,4 +2,12 @@ package christmas.benefits;
 
 public class MerchandiseBenefit {
 
+    private final int CHAMPAGNE_DESERVED = 120_000;
+
+    public boolean champagneDeserved(int totalOrder){
+        if(totalOrder > CHAMPAGNE_DESERVED){
+            return true;
+        }
+        return false;
+    }
 }
