4e01134e31b680142c24fc2a8e9346bd3284a033
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 02:14:37 +0900

    docs : 출력 관련 역할별 함수 생성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,7 +4,32 @@ import christmas.benefits.AccumulateDiscount;
 
 public class OutputView {
 
+    public void printOrders(){
+
+    }
+
+    public void printTotalPrice(){
+
+    }
+
+    public void printMerchandise(){
+
+    }
+
     public void printAllBenefit(AccumulateDiscount accumulateDiscount){
 
     }
+
+    public void printTotalBenefit(){
+
+    }
+
+    public void printExpectedPrice(){
+
+    }
+
+    public void printEventBadge(){
+
+    }
+
 }
