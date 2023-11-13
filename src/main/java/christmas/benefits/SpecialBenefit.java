c6e0205f5bb4d961d122e2f224c77aa8aaa6d779
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 00:35:39 +0900

    feat(SpecialBenefit) : 별표 표시된 날짜에 추가 할인 기능 구현

--- src/main/java/christmas/benefits/SpecialBenefit.java
+++ src/main/java/christmas/benefits/SpecialBenefit.java
@@ -1,4 +1,19 @@
 package christmas.benefits;
 
+import christmas.eventdate.EventDateUtils;
+
 public class SpecialBenefit {
+
+    private EventDateUtils eventDateUtils;
+
+    public SpecialBenefit(EventDateUtils eventDateUtils) {
+        this.eventDateUtils = eventDateUtils;
+    }
+
+    public int starMarkedBenefit(int day){
+        final int STAR_MARKED_DISCOUNT = 1000;
+
+        if(eventDateUtils.isStarMarked(day));
+        return STAR_MARKED_DISCOUNT;
+    }
 }
