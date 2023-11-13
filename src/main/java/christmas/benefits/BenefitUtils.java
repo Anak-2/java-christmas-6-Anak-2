d6271653ab629ee11aa142c7a0d78163ee924cf2
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 00:59:53 +0900

    feat(BenefitUtils) : 날짜 관련 혜택 계산 기능 구현

--- src/main/java/christmas/benefits/BenefitUtils.java
+++ src/main/java/christmas/benefits/BenefitUtils.java
@@ -8,16 +8,38 @@ public class BenefitUtils {
 
     private final int EVENT_TARGET_CONDITION = 10_000;
 
+    private final DateBenefit dateBenefit;
+    private final SpecialBenefit specialBenefit;
+    private final MerchandiseBenefit merchandiseBenefit;
+
+    public BenefitUtils(DateBenefit dateBenefit, SpecialBenefit specialBenefit,
+                        MerchandiseBenefit merchandiseBenefit) {
+        this.dateBenefit = dateBenefit;
+        this.specialBenefit = specialBenefit;
+        this.merchandiseBenefit = merchandiseBenefit;
+    }
+
     public boolean isEventTarget(int totalOrder){
         return (totalOrder >= EVENT_TARGET_CONDITION);
     }
 
-    public boolean calculateBenefit(List<Order> orders){
+    public int calculateBenefit(List<Order> orders, int day){
+        int totalPrice = Order.totalPrice(orders);
         // 혜택 대상인지 확인
+        if(!isEventTarget(totalPrice)){
+            return 0;
+        }
         // 날짜 관련 혜택 확인
+        int dateBenefit = calculateDateBenefit(orders, day);
         // 증정 혜택 관련 확인
         // 특별 혜택 관련 확인
         // 샴페인 대상인지 확인
-        return false;
+        return 0;
+    }
+
+    public int calculateDateBenefit(List<Order> orders, int day){
+        int dDayBenefit = dateBenefit.calculateBenefitByDDay(day);
+        int weekBenefit = dateBenefit.calculateBenefitByWeekend(orders, day);
+        return Math.addExact(dDayBenefit, weekBenefit);
     }
 }
