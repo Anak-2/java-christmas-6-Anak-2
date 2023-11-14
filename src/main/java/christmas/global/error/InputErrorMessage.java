429f25d4ab4b8064c0c3c3f58c4e4838ce2e7667
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 23:10:09 +0900

    chore(InputErrorMessage) : 음료만 주문시 안내 문구 수정

--- src/main/java/christmas/global/error/InputErrorMessage.java
+++ src/main/java/christmas/global/error/InputErrorMessage.java
@@ -6,7 +6,7 @@ public enum InputErrorMessage {
     INVALID_MENU_COUNT("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     EXCEED_MAX_MENU_COUNT("[ERROR] 한 번 주문 메뉴 최대 개수는 20개입니다. 다시 입력해 주세요"),
     DUPLICATED_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    NO_ALL_BEVERAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    NO_ALL_BEVERAGE("[ERROR] 음료만 주문은 불가능합니다. 다시 입력해 주세요.");
 
     private String message;
 
