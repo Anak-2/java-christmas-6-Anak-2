fe5022134a92da8b20d30491c70b9ab6be92df7d
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-14 16:54:20 +0900

    test(InputValidator) : 날짜 예외 관련 기능 테스트

--- src/test/java/christmas/view/InputValidatorTest.java
+++ src/test/java/christmas/view/InputValidatorTest.java
@@ -1,5 +1,8 @@
 package christmas.view;
 
+import christmas.config.ChristmasConfig;
+import christmas.manager.ChristmasEventManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class InputValidatorTest {
 
     @Test
+    @DisplayName("날짜 오류 기능 테스트")
     void validDate() {
+        String invalidInputDate1 = "a";
+        String invalidInputDate2 = "32";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> InputValidator.validDate(invalidInputDate1));
+        assertThrows(IllegalArgumentException.class,
+                () -> InputValidator.validDate(invalidInputDate2));
     }
 
     @Test
